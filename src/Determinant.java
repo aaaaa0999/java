c3a1402504f76713b8f488308ed08d592e07f237
aaaaa0999 <vbzwkk@gmail.com>
2024-05-10 02:20:15 +0300

    Update Determinant.java

--- src/Determinant.java
+++ src/Determinant.java
@@ -1,28 +1,4 @@
-import java.io.File;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 import parcs.*;
-
-class Matrix implements Serializable {
-    private double[][] data;
-
-    public Matrix(int size) {
-        data = new double[size][size];
-    }
-
-    public void setData(int row, int col, double value) {
-        data[row][col] = value;
-    }
-
-    public double[][] getData() {
-        return data;
-    }
-
-    public int getSize() {
-        return data.length;
-    }
-}
 
 public class Determinant implements AM {
     public void run(AMInfo info) {
@@ -52,34 +28,4 @@ public class Determinant implements AM {
         }
         return det;
     }
-}
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        task curtask = new task();
-        curtask.addJarFile("Determinant.jar");
-        Matrix matrix = fromFile(curtask.findFile("input"));
-
-        AMInfo info = new AMInfo(curtask, null);
-        point p = info.createPoint();
-        channel c = p.createChannel();
-        p.execute("Determinant");
-        c.write(matrix);
-
-        System.out.println("Waiting for result...");
-        System.out.println("Result: " + c.readDouble());
-        curtask.end();
-    }
-
-    public static Matrix fromFile(String filename) throws Exception {
-        Scanner sc = new Scanner(new File(filename));
-        int size = sc.nextInt();
-        Matrix matrix = new Matrix(size);
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix.setData(i, j, sc.nextDouble());
-            }
-        }
-        return matrix;
-    }
-}
