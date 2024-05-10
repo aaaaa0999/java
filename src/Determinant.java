042e21cb15558f35dc87e6dd6d47d89c751d4b6b
aaaaa0999 <vbzwkk@gmail.com>
2024-05-10 05:01:02 +0300

    Update Determinant.java

--- src/Determinant.java
+++ src/Determinant.java
@@ -3,37 +3,47 @@ import parcs.*;
 public class Determinant implements AM {
     public void run(AMInfo info) {
         try {
-            System.out.println("Determinant calculation started.");
+            System.out.println("Gaussian elimination started.");
             Matrix matrix = (Matrix) info.parent.readObject();
             System.out.println("Received matrix for processing.");
-            double result = calculateDeterminant(matrix.getData());
-            System.out.println("Calculated determinant: " + result);
+            double result = gaussianDeterminant(matrix.getData());
+            System.out.println("Calculated determinant using Gaussian elimination: " + result);
             info.parent.write(result);
             System.out.println("Result sent back to parent.");
         } catch (Exception e) {
-            System.err.println("Error during determinant calculation: " + e.getMessage());
+            System.err.println("Error during Gaussian elimination: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private double calculateDeterminant(double[][] matrix) {
+    private double gaussianDeterminant(double[][] matrix) {
         int size = matrix.length;
-        if (size == 1) {
-            return matrix[0][0];
-        }
-        double det = 0;
+        double det = 1;
         for (int i = 0; i < size; i++) {
-            double[][] subMatrix = new double[size - 1][size - 1];
-            for (int j = 1; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    if (k < i) {
-                        subMatrix[j - 1][k] = matrix[j][k];
-                    } else if (k > i) {
-                        subMatrix[j - 1][k - 1] = matrix[j][k];
-                    }
+            int maxRow = i;
+            for (int j = i + 1; j < size; j++) {
+                if (Math.abs(matrix[j][i]) > Math.abs(matrix[maxRow][i])) {
+                    maxRow = j;
                 }
             }
-            det += Math.pow(-1, i) * matrix[0][i] * calculateDeterminant(subMatrix);
+            if (i != maxRow) {
+                double[] temp = matrix[i];
+                matrix[i] = matrix[maxRow];
+                matrix[maxRow] = temp;
+                det *= -1;
+            }
+
+            if (matrix[i][i] == 0) {
+                return 0;
+            }
+
+            det *= matrix[i][i];
+            for (int j = i + 1; j < size; j++) {
+                double factor = matrix[j][i] / matrix[i][i];
+                for (int k = i; k < size; k++) {
+                    matrix[j][k] -= factor * matrix[i][k];
+                }
+            }
         }
         return det;
     }
